b5930d6d795aa44f69c9ad2b45617e0d1bd4ec8b
NAME <mail@gmail.com>
2023-09-02 14:04:14 +0530

    made env dynamic

--- src/lib/server/plaidClient.js
+++ src/lib/server/plaidClient.js
@@ -1,19 +1,20 @@
-import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
-import { PLAID_CLIENT_ID, PLAID_SECRET, PLAID_ENV } from '$env/static/private';
+import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
+import { env } from "$env/dynamic/private";
 
+const { PLAID_CLIENT_ID, PLAID_SECRET, PLAID_ENV } = env;
 const configuration = new Configuration({
-	basePath:
-		PLAID_ENV === 'production'
-			? PlaidEnvironments.production
-			: PLAID_ENV === 'development'
-			? PlaidEnvironments.development
-			: PlaidEnvironments.sandbox,
-	baseOptions: {
-		headers: {
-			'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
-			'PLAID-SECRET': PLAID_SECRET
-		}
-	}
+  basePath:
+    PLAID_ENV === "production"
+      ? PlaidEnvironments.production
+      : PLAID_ENV === "development"
+      ? PlaidEnvironments.development
+      : PlaidEnvironments.sandbox,
+  baseOptions: {
+    headers: {
+      "PLAID-CLIENT-ID": PLAID_CLIENT_ID,
+      "PLAID-SECRET": PLAID_SECRET,
+    },
+  },
 });
 
 const client = new PlaidApi(configuration);
