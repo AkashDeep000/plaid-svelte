f98ae641b1f684b222a2fae6b19904dd4181ca5b
NAME <mail@gmail.com>
2023-09-02 14:42:50 +0530

    added vercel kv

--- src/lib/server/dbClient.js
+++ src/lib/server/dbClient.js
@@ -1,12 +1,15 @@
-import ReplitDB from '@replit/database';
-import JSONdb from 'simple-json-db';
-import { env } from '$env/dynamic/private';
+import ReplitDB from "@replit/database";
+import JSONdb from "simple-json-db";
+import kv from "@vercel/kv";
+import { env } from "$env/dynamic/private";
 
 let db;
 
 if (env.REPLIT_DB_URL) {
-	db = new ReplitDB();
+  db = new ReplitDB();
+} else if (env.VERCEL_ENV) {
+  db = kv;
 } else {
-	db = new JSONdb('./db.json');
+  db = new JSONdb("./db.json");
 }
 export default db;
